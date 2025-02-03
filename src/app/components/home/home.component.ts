70fcb0a503efdd3318b0c4fd89fd60f71ea34dbd
Dávid Törő <89945740+dtoro97@users.noreply.github.com>
2025-02-03 02:03:32 +0100

    updated home page to use signals only

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,24 +1,21 @@
 import { CarouselModule } from 'primeng/carousel';
 import { SelectButtonModule } from 'primeng/selectbutton';
-import {
-  BehaviorSubject,
-  first,
-  from,
-  map,
-  Observable,
-  switchMap,
-  tap,
-} from 'rxjs';
+import { from, map } from 'rxjs';
 import { Movie, PopularTvShowResult, TimeWindow } from 'tmdb-ts';
 
 import { AsyncPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  signal,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { CAROUSEL_BREAKPOINTS } from '../../carousel-breakpoints';
-import { CardComponent } from '../card/card.component';
-import { TmdbService } from '../../shared';
 import { StateService } from '../../core';
+import { TmdbService } from '../../shared';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-home',
@@ -33,10 +30,12 @@ import { StateService } from '../../core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit {
-  trending$: Observable<any[]>;
-  popular$: Observable<Movie[] | PopularTvShowResult[]>;
-  upcomingMovies$: Observable<Movie[]>;
+export class HomeComponent {
+  trending = signal<any>(undefined);
+  popular = signal<Movie[] | PopularTvShowResult[]>([]);
+  upcomingMovies = signal<Movie[]>([]);
+  selectedTrending = signal<string>('day');
+  selectedPopular = signal<string>('tv');
   breakpoints = CAROUSEL_BREAKPOINTS;
   trendingOptions = [
     { label: 'Today', value: 'day' },
@@ -52,52 +51,46 @@ export class HomeComponent implements OnInit {
       value: 'movie',
     },
   ];
-  selectedTrending$: Observable<string>;
-  selectedPopular$: Observable<string>;
-  private _selectedTrending: BehaviorSubject<string> = new BehaviorSubject(
-    'day'
-  );
-  private _selectedPopular: BehaviorSubject<string> = new BehaviorSubject('tv');
-  constructor(private tmdb: TmdbService, private stateService: StateService) {}
-  ngOnInit(): void {
-    this.selectedTrending$ = this._selectedTrending.asObservable();
-    this.selectedPopular$ = this._selectedPopular.asObservable();
-    this.trending$ = this.selectedTrending$.pipe(
-      switchMap((timeWindow) => {
-        return from(
-          this.tmdb.trending.trending('all', timeWindow as TimeWindow)
-        );
-      }),
-      map((data) => data.results),
-      tap(() => this.stateService.setLoading(false))
-    );
-    this.upcomingMovies$ = from(this.tmdb.movies.upcoming()).pipe(
-      first(),
-      map((data) => data.results || [])
-    );
+  constructor(private tmdb: TmdbService, private stateService: StateService) {
+    effect(() => {
+      const popular = this.selectedPopular();
+      from(
+        popular === 'tv'
+          ? this.tmdb.tvShows.popular()
+          : this.tmdb.movies.popular()
+      )
+        .pipe(map((data) => data.results))
+        .subscribe((data) => {
+          this.popular.set(data);
+          this.stateService.setLoading(false);
+        });
+    });
 
-    this.popular$ = this.selectedPopular$.pipe(
-      switchMap((mediaType) => {
-        if (mediaType === 'tv') {
-          return from(this.tmdb.tvShows.popular());
-        }
-        return this.tmdb.movies.popular();
-      }),
-      map((data) => data.results),
-      tap(() => this.stateService.setLoading(false))
-    );
+    effect(() => {
+      const trending = this.selectedTrending();
+      from(this.tmdb.trending.trending('all', trending as TimeWindow))
+        .pipe(map((data) => data.results))
+        .subscribe((data) => {
+          this.trending.set(data);
+          this.stateService.setLoading(false);
+        });
+    });
+
+    from(this.tmdb.movies.popular())
+      .pipe(map((data) => data.results))
+      .subscribe((data) => this.upcomingMovies.set(data));
   }
 
   changeTrending(timeWindow: string) {
-    if (timeWindow && this._selectedTrending.value !== timeWindow) {
+    if (timeWindow && this.selectedTrending() !== timeWindow) {
       this.stateService.setLoading(true);
-      this._selectedTrending.next(timeWindow);
+      this.selectedTrending.set(timeWindow);
     }
   }
   changePopular(mediaType: string) {
-    if (mediaType && this._selectedPopular.value !== mediaType) {
+    if (mediaType && this.selectedPopular() !== mediaType) {
       this.stateService.setLoading(true);
-      this._selectedPopular.next(mediaType);
+      this.selectedPopular.set(mediaType);
     }
   }
 }
